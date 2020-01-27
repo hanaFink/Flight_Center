bd56e781756a8b7720ada985b0be1fb89c5b75e7
zloyfrank <zloyfrank@gmail.com>
2020-01-27 18:27:48 +0200

    update 27/01/20 18:28

---
+++ Flight_Center/AnonymousUserFacade.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flight_Center
+{
+    class AnonymousUserFacade : FacadeBase, IAnonymousUserFacade
+    {
+        public IList<AirlineCompanies> GetAllAirlineCompanies()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Flights> GetAllFlights()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<Flights, int> GetAllFlightsVacancy()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Flights GetFlightById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Flights> GetFlightsByDepartureDate(DateTime departureDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Flights> GetFlightsByDestinationCountry(int countryCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Flights> GetFlightsByLandingDate(DateTime landingDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Flights> GetFlightsByOriginCountry(int countryCode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+ 
+    
+}
---
+++ Flight_Center/FacadeBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flight_Center
+{
+    abstract class FacadeBase
+    {
+        protected IAirlineDAO _airlineDao;
+        protected ICountryDAO _countryDao;
+        protected ICustomerDAO _customerDAO;
+        protected IFlightDAO _flightDAO;
+        protected ITicketDAO _ticketDAO;
+
+    }
+}
